aed69808f5dcdaf3266686a7af9be26457cdf0a6
Camila Palomino <1camilaguerra@gmail.com>
2022-06-09 20:40:56 -0300

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 
 const SignIn = () => {
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID_KEY
-  const REDIRECT_URI = 'https://spotify-color.vercel.app/webapp'
+  const REDIRECT_URI = 'https://spotify-color.vercel.app/webapp' 
   var STATE = Date.now()
   var SCOPE = 'user-read-private user-read-email user-top-read user-follow-read'
   const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+
+  console.log(REDIRECT_URI)
 
   return (
     <>
